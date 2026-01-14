424d7d1d4bff638c1249bec0854eae55551c83d0
LeanFicagna <leandersonficagna321@gmail.com>
2026-01-14 09:23:39 -0300

    refactor: Remoção bootstrap slide

--- script/app.js
+++ script/app.js
@@ -32,5 +32,32 @@ const toggle = document.querySelector('.navbar-toggle-s');
 const menu = document.querySelector('.navbar-menu-s');
 
 toggle.addEventListener('click', () => {
-menu.classList.toggle('active');
+    menu.classList.toggle('active');
 });
+
+const slides = document.querySelectorAll('.slide');
+const track = document.querySelector('.slider-track');
+const nextBtn = document.querySelector('.slider-btn.next');
+const prevBtn = document.querySelector('.slider-btn.prev');
+
+let index = 0;
+
+function updateSlide() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    updateSlide();
+});
+
+prevBtn.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    updateSlide();
+});
+
+// autoplay
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    updateSlide();
+}, 10000);
