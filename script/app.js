79fc892b35987d529b8a45d713b952c9edea8f90
LeanFicagna <leandersonficagna321@gmail.com>
2026-01-20 17:52:57 -0300

    refactors: Init refatoração css e javascript

--- script/app.js
+++ script/app.js
@@ -6,28 +6,7 @@ function toggleMenu() {
         nav.style.display = "flex";
     }
 }
-let currentSlide = 0;
-function showSlide(index) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-    if (index >= totalSlides) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = totalSlides - 1;
-    } else {
-        currentSlide = index;
-    }
-    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
-}
-function nextSlide() {
-    currentSlide++;
-    showSlide(currentSlide);
-}
-function prevSlide() {
-    currentSlide--;
-    showSlide(currentSlide);
-}
-/*
+
 const toggle = document.querySelector('.navbar-toggle-s');
 const menu = document.querySelector('.navbar-menu-s');
 
@@ -35,32 +14,6 @@ toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-const slides = document.querySelectorAll('.slide');
-const track = document.querySelector('.slider-track');
-const nextBtn = document.querySelector('.slider-btn.next');
-const prevBtn = document.querySelector('.slider-btn.prev');
-
-let index = 0;
-
-function updateSlide() {
-    track.style.transform = `translateX(-${index * 100}%)`;
-}
-
-nextBtn.addEventListener('click', () => {
-    index = (index + 1) % slides.length;
-    updateSlide();
-});
-
-prevBtn.addEventListener('click', () => {
-    index = (index - 1 + slides.length) % slides.length;
-    updateSlide();
-});
-*/
-// autoplay
-setInterval(() => {
-    index = (index + 1) % slides.length;
-    updateSlide();
-}, 10000);
 // verifica e aplica a máscara de telefone no campo de entrada do entre em contato
 const handlePhone = (event) => {
   let input = event.target
