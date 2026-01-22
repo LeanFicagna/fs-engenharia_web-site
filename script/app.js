26431f904d3c69c90daa2ef6dfe5b04ad31e3037
Igo Quintino <igocastro.15@gmail.com>
2026-01-22 02:49:47 -0300

    cards do serivos quase completo, estão resposivos mas precisa de ajuste no alimento dentro do body em si, add no script o comportamento do swiper e por fim, add no html a cdn e script do swiperJS para esse slide by slide feito

--- script/app.js
+++ script/app.js
@@ -1,51 +1,93 @@
+
 function toggleMenu() {
-    var nav = document.getElementById("nav-menu");
-    if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-    }
+  var nav = document.getElementById("nav-menu");
+  if (nav.style.display === "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
 }
 
-const toggle = document.querySelector('.navbar-toggle-s');
-const menu = document.querySelector('.navbar-menu-s');
+const toggle = document.querySelector(".navbar-toggle-s");
+const menu = document.querySelector(".navbar-menu-s");
 
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
 
 // verifica e aplica a máscara de telefone no campo de entrada do entre em contato
 const handlePhone = (event) => {
-  let input = event.target
-  input.value = phoneMask(input.value)
-}
+  let input = event.target;
+  input.value = phoneMask(input.value);
+};
 
 const phoneMask = (value) => {
-  if (!value) return ""
-  value = value.replace(/\D/g, '') // Remove tudo o que não for número
-  value = value.replace(/(\d{2})(\d)/, "($1) $2") // Coloca parênteses nos dois primeiros dígitos
-  value = value.replace(/(\d{5})(\d)/, "$1-$2") // Coloca o traço após o quinto dígito
-  return value
-}
-
+  if (!value) return "";
+  value = value.replace(/\D/g, ""); // Remove tudo o que não for número
+  value = value.replace(/(\d{2})(\d)/, "($1) $2"); // Coloca parênteses nos dois primeiros dígitos
+  value = value.replace(/(\d{5})(\d)/, "$1-$2"); // Coloca o traço após o quinto dígito
+  return value;
+};
 
 /* Hero */
 
-const hero = document.querySelector('.hero');
+const hero = document.querySelector(".hero");
 
-  const backgrounds = [
-    'img/slide-mapeamento-lg.png',
-    'img/slide-credito-rural-lg.png',
-    'img/slide-geoprocessamento-lg.png'
-  ];
+const backgrounds = [
+  "img/slide-mapeamento-lg.png",
+  "img/slide-credito-rural-lg.png",
+  "img/slide-geoprocessamento-lg.png",
+];
 
-  let index = 0;
+let index = 0;
 
-  function changeBackground() {
-    hero.style.backgroundImage = `url(${backgrounds[index]})`;
-    index = (index + 1) % backgrounds.length;
-  }
+function changeBackground() {
+  hero.style.backgroundImage = `url(${backgrounds[index]})`;
+  index = (index + 1) % backgrounds.length;
+}
 
-  // Comportamento da imagem do slide
-  changeBackground(); // primeira imagem
-  setInterval(changeBackground, 6000); // troca a cada 6s
+// Comportamento da imagem do slide
+changeBackground(); // primeira imagem
+setInterval(changeBackground, 6000); // troca a cada 6s
+
+
+
+// Configuracao do swiper
+new Swiper('.card-wrapper', {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 30,
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        675: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+        1280: {
+            slidesPerView: 4
+        },
+    }
+
+});
